0a1fd30fcabba2dbc7cb1a908d0c627c619b848a
aryprabow <57415661+aryprabow@users.noreply.github.com>
2021-11-10 22:16:15 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Script By Brody"
+echo "Script By BOWO"
 clear
 echo -e ""
 echo -e "======================================"
