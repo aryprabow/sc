5507b5a718039bb0bad8e35a1e437c894c16736d
aryprabow <57415661+aryprabow@users.noreply.github.com>
2021-11-10 22:15:30 +0700

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Script By SSH SEDANG"
+echo "Script By BOWO"
 clear
 if [[ "$IP2" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
@@ -72,5 +72,5 @@ echo -e "Created  : $created"
 echo -e "Expired   : $exp"
 echo -e "==============================="
 echo -e ""
-echo -e "Script By Brody"
+echo -e "Script By BOWO"
 echo -e ""
